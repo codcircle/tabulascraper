39b208c8fcc3af2efcc862a58eb6400e6ad0eb52
Erik <83910726+jserik@users.noreply.github.com>
2021-09-28 18:09:44 +0200

    v1.0

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
-print('Hi')
+# modules 
+import requests
+from bs4 import BeautifulSoup
+from html.parser import HTMLParser
+from datetime import datetime
+
+# variables
+URL = 'http://kgazugang:HalloFasanerie@tabula.kronberg-gymnasium.de/secure/export.html?'
+now = datetime.now()
+date = now.strftime("%d_%m_%Y %H_%M_%S")
+
+file1 = open("{date}.txt","w")
+class MyHTMLParser(HTMLParser):
+    def handle_data(self, data):
+        print(data)
+        file1.write(data)
+
+parser = MyHTMLParser()
+page = requests.get(URL).text
+
+soup = BeautifulSoup(page, "html.parser")
+
+def parse_data():
+    for row in soup.find_all("tr"):
+        cells = str(row.find_all("td"))
+        parser.feed(cells)
+
+parse_data()
+
+
+
